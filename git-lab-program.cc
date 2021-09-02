3349adbddfd9226d47b1959a0824c14c20d06488
Robert Matters <rm340715@ohio.edu>
2021-09-02 16:52:28 -0400

    git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Robert Matters>
+ *        Date: <9-2-2021>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
